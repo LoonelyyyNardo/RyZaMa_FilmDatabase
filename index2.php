23d0559716d87b4af53a36746582200bb4ec46cd
Rygulski Martin <147497442+LoonelyyyNardo@users.noreply.github.com>
2025-06-05 09:46:58 +0200

    first commit
    basic site with navbar and styles

---
+++ index2.php
@@ -0,0 +1,7 @@
+<?php
+echo $this->extend('layout');
+echo $this->section('content');
+
+
+
+echo $this->endSection();
---
+++ navbar.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background: rgb(240,0,50);
+  
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: white;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: black;
+  color: white;
+}
+
+.nav-left a,
+.nav-right a {
+    color: white;
+    margin: 0 10px;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.nav-left a:hover,
+.nav-right a:hover {
+    text-decoration: underline;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="<?php echo('') ?>">Hlavní stránka</a>
+      <a class="nav-link nav-left" href="#">Filmy</a>
+      <a class="nav-link nav-left" href="#">Herci</a>
+      <a class="nav-link nav-left" href="#">Režiséři</a>
+      <a class="nav-link nav-left" href="#">Recenze</a>
+      <a class="nav-link nav-right" href="#">Přihlásit se</a>
+</div>
+</body>
+</html>
